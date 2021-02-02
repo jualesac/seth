eefd51efdc053a8a03dca17395e8d0fb8915b813
Julio Alejandro Santos Corona <59350645+jualesac@users.noreply.github.com>
2021-02-02 00:56:35 -0600

    Create obtenerMenu.sql

---
+++ procedures/obtenerMenu.sql
@@ -0,0 +1,30 @@
+/*
+ * FECHA: 2020/08/27
+ * AUTOR: Julio Alejandro Santos Corona
+ * CORREO: jualesac@yahoo.com
+ * TÍTULO: obtenerMenu.sql
+ *
+ * Descripción: Procedimiento que obtiene los menus activos para el usuario
+*/
+
+DROP PROCEDURE IF EXISTS obtenerMenu;
+
+DELIMITER //
+
+/*CREATE PROCEDURE obtenerMenu(IIDUSUARIO INT(11)) /*Utilizar si se utilizará el idUsuario*/
+CREATE PROCEDURE obtenerMenu(USSUARIO VARCHAR(7))
+BEGIN
+    SELECT DISTINCT
+    `apps`.`idApp`,
+    `apps`.`nombre`,
+    `apps`.`ruta`
+    FROM `apps`
+    INNER JOIN `mapa` ON `mapa`.`ruta` LIKE CONCAT(`apps`.`ruta`, "%")
+    WHERE
+    `apps`.`visible` = 1
+    /*AND `idUsuario` = IIDUSUARIO /*Activar si se va a utilizar el idUsuario*/
+    AND `usuario` = BINARY(USSUARIO)
+    AND `mapa`.`estatus` = 1;
+END; //
+
+DELIMITER ;
